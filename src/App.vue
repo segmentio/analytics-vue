81891e3943edae382be0a34e44b74ad3f141d8fa
William Grosset <william.grosset@segment.com>
2019-01-10 22:06:19 -0800

    Add Quickstart for Analytics.js + Vue.js

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Link />
+  </div>
+</template>
+
+<script>
+import Link from './components/Link'
+
+export default {
+  name: 'App',
+  components: {
+    Link 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin: 140px;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Link.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Welcome to Your Vue.js App</h1>
+    <a
+      href="https://vuejs.org"
+      target="_blank"
+      v-on:click="trackClickEvent"
+     >
+      {{ msg }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Learn Vue'
+    }
+  },
+  methods: {
+    trackClickEvent () {
+      analytics.track('Clicked Learn Vue Link') 
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+  font-size: 22px;
+}
+</style>
