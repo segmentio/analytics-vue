6876b1654f0bb0b65002bd9782c7af3be88d8b0b
William Grosset <william.grosset@segment.com>
2019-01-22 11:54:08 -0800

    Intergrate vue-router and add page components
    + Create index.js for configuring routes
    + Condense scope (template) of <Link />
    + Add 2 page components: <Home />, <About />

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
+    <router-link class="router-link" to="/">Home</router-link>
+    <router-link class="router-link" to="/about">About</router-link>
+    <router-view></router-view>
     <img src="./assets/logo.png">
+    <p>Edit <code>src/App.vue</code> and save to reload.</p>
     <Link />
   </div>
 </template>
 
 <script>
-import Link from './components/Link'
+import Link from '@/components/Link'
 
 export default {
   name: 'App',
   components: {
-    Link 
+    Link
   }
 }
 </script>
@@ -24,5 +28,15 @@ export default {
   text-align: center;
   margin: 140px;
   color: #2c3e50;
+}
+
+#app > p {
+  font-size: 28px;
+}
+
+.router-link {
+  color: #42b983;
+  padding: 30px;
+  font-size: 24px;
 }
 </style>
